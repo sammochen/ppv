99fe5317df0aab884face4bab14b95485bc46349
Shih-Hao Chen <samuelshchen@gmail.com>
2022-04-10 10:44:25 +1000

    control textfield

--- packages/frontend/src/components/WordInput.tsx
+++ packages/frontend/src/components/WordInput.tsx
@@ -1,52 +1,57 @@
 import {Grid, Paper, TextField, Typography} from '@mui/material';
 import {styled} from '@mui/material/styles';
-import React from 'react';
+import React, {useState} from 'react';
 import {
   popColor as secondaryColor,
   secondaryColor as popColor,
   textColor,
 } from '../theme/colors';
 
+const titleFontSize = '1.5rem';
+const titleFontWeight = 550;
+const definitionFontSize = '0.9rem';
+
+const StyledTextField = styled(TextField)({
+  '& .MuiOutlinedInput-root': {
+    '& input': {
+      color: textColor.hex(),
+      fontSize: titleFontSize,
+      fontWeight: titleFontWeight,
+    },
+
+    '& fieldset': {
+      border: `1.5px solid ${secondaryColor.hex()}`,
+    },
+
+    '&:hover fieldset': {
+      border: `1.5px solid ${secondaryColor.hex()}`,
+    },
+
+    '&.Mui-focused fieldset': {
+      border: `2px solid ${secondaryColor.hex()}`,
+    },
+  },
+  '& .MuiInputLabel-root': {
+    color: secondaryColor.hex(),
+
+    '&.Mui-focused': {
+      color: secondaryColor.hex(),
+    },
+  },
+});
+
 export type WordInputProps = {
   onSubmitWord: (word: string) => void;
 };
 export const WordInput = ({onSubmitWord}: WordInputProps) => {
-  const titleFontSize = '1.5rem';
-  const titleFontWeight = 550;
-  const definitionFontSize = '0.9rem';
-  const metaFontSize = '0.8rem';
-  const offset = '0.6rem';
-
+  const [textValue, setTextValue] = useState('');
   const wordObj = {word: 'hi', definitions: [], author: '', date: Date.now()};
 
-  const CssTextField = styled(TextField)({
-    '& .MuiOutlinedInput-root': {
-      '& input': {
-        color: textColor.hex(),
-        fontSize: titleFontSize,
-        fontWeight: titleFontWeight,
-      },
-
-      '& fieldset': {
-        border: `1.5px solid ${secondaryColor.hex()}`,
-      },
-
-      '&:hover fieldset': {
-        border: `1.5px solid ${secondaryColor.hex()}`,
-      },
-
-      '&.Mui-focused fieldset': {
-        border: `2px solid ${secondaryColor.hex()}`,
-      },
-    },
-    '& .MuiInputLabel-root': {
-      color: secondaryColor.hex(),
-
-      '&.Mui-focused': {
-        color: secondaryColor.hex(),
-      },
-    },
-  });
+  const handleTextFieldChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setTextValue(event.target.value);
+  };
 
   return (
     <Paper
@@ -59,7 +64,7 @@ export const WordInput = ({onSubmitWord}: WordInputProps) => {
       {/* Main word  */}
       <Grid container sx={{padding: '1rem'}}>
         <Grid item xs={12}>
-          <CssTextField
+          <StyledTextField
             sx={{
               fontSize: titleFontSize,
               fontWeight: titleFontWeight,
@@ -70,9 +75,11 @@ export const WordInput = ({onSubmitWord}: WordInputProps) => {
             }}
             fullWidth
             label={'Word'}
+            value={textValue}
+            onChange={handleTextFieldChange}
           >
             {wordObj.word}
-          </CssTextField>
+          </StyledTextField>
 
           {wordObj.definitions.map((definition, index) => {
             return (
